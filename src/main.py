e26d5e28bbef2cd5d16e29db732c4d95418d352d
Ben Coleman <colemanb@moravian.edu>
2018-03-28 06:23:18 -0400

    Organize project in standard structure

---
+++ src/main.py
@@ -0,0 +1,8 @@
+
+
+def square(x):
+    return x * x
+
+
+if __name__ == '__main__':
+    print(square(5))
---
+++ tests/test_main.py
@@ -0,0 +1,18 @@
+
+from main import square
+
+
+def test_zero():
+    assert square(0) == 0
+
+
+def test_positive():
+    assert square(5) == 25
+    assert square(1) == 1
+    assert square(1000) == 1000000
+
+
+def test_negative():
+    assert square(-5) == 25
+    assert square(-1) == 1
+    assert square(-1000) == 1000000
