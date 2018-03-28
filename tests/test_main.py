4a311b86853fedb7949eb08c452a26a3bb57c3b2
Ben Coleman <colemanb@moravian.edu>
2018-03-28 07:17:51 -0400

    Restructure for tox

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,18 +1,18 @@
 
-from main import square
+from citest import main
 
 
 def test_zero():
-    assert square(0) == 0
+    assert main.square(0) == 0
 
 
 def test_positive():
-    assert square(5) == 25
-    assert square(1) == 1
-    assert square(1000) == 1000000
+    assert main.square(5) == 25
+    assert main.square(1) == 1
+    assert main.square(1000) == 1000000
 
 
 def test_negative():
-    assert square(-5) == 25
-    assert square(-1) == 1
-    assert square(-1000) == 1000000
+    assert main.square(-5) == 25
+    assert main.square(-1) == 1
+    assert main.square(-1000) == 1000000
